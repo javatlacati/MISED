822464e1d8a56c0e0a3242266b6eb1205765a3ff
Ruslan López <scherzo_16@hotmail.com>
2017-04-26 01:46:01 -0500

    Method nomeclature standarization and documentation improvements.

--- src/main/java/gob/senado/ppf/sed/utilidades/Formateable.java
+++ src/main/java/gob/senado/ppf/sed/utilidades/Formateable.java
@@ -2,9 +2,16 @@ package gob.senado.ppf.sed.utilidades;
 
 import com.google.gson.Gson;
 
+/**
+ * Permite formatear una clase como JSON.
+ * Provee una implementaci&oacute;n por defecto basada en GSON.
+ */
 public interface Formateable {
 
-	default String json() {
-		return new Gson().toJson(this);
-	}
+    /**
+     * Obtiene una representaci&oacute;n de la instancia del objeto actual en formato JSON.
+     */
+    default String json() {
+        return new Gson().toJson(this);
+    }
 }
