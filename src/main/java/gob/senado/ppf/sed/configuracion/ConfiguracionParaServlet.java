bda594fce7ac51da6e32c915fc69b0f132a56573
Benjamin <Benjamin@192.168.0.33>
2017-04-26 01:46:29 -0500

    Nuevas clases de configuracion aun no implementadas pero estan contempladas para su uso, baja de programas institucionales(aun no realtime, esperando a configurar websocket para realizarlo, plano de javascript para Prototypes de tipo programa institucional.

--- src/main/java/gob/senado/ppf/sed/configuracion/ConfiguracionParaServlet.java
+++
@@ -1,8 +0,0 @@
-package gob.senado.ppf.sed.configuracion;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ConfiguracionParaServlet {
-
-}
---
+++ src/main/webapp/resources/js/funciones/prototipos/programa-institucional.js
@@ -0,0 +1,20 @@
+/**
+ * Funcion que construye un prototype ProgramaInstitucional para almacenar el identificador del programa institucional,
+ * la clave del programa institucional, el nombre del programa institucional y la descripcion del mismo.
+ * @param pIdProgramaInstitucional identificador del programa
+ * @param pClave clave del programa
+ * @param pNombre nombre del programa
+ * @param pDescripcion descripcion del programa
+ */
+function ProgramaInstitucional(pIdProgramaInstitucional = "", pClave = "", pNombre = "", pDescripcion = ""){
+	this.idProgramaInstitucional = pIdProgramaInstitucional;
+	this.clave = pClave;
+	this.nombre = pNombre;
+	this.descripcion = pDescripcion;
+}
+
+ProgramaInstitucional.prototype.toString = function(){
+	return this.idProgramaInstitucional + " " + this.clave + " " + this.nombre + " " + this.descripcion;
+};
+
+
