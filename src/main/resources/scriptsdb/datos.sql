0a79b2c4c018e288c773bda50a6300f7173adfc3
Ruslan López <scherzo_16@hotmail.com>
2017-05-02 01:38:04 -0500

    Corrección de script de datos

--- src/main/resources/scriptsdb/datos.sql
+++ src/main/resources/scriptsdb/datos.sql
@@ -38,23 +38,21 @@ INSERT INTO UNIDAD_APOYO VALUES(25, 1,'Instituto Belisario Domínguez', '');
 INSERT INTO UNIDAD_APOYO VALUES(26, 1,'Coordinación de Comunicación Social', '');
 INSERT INTO UNIDAD_APOYO VALUES(27, 1,'Canal del Congreso', '');
 
-SELECT * FROM USUARIO U
-INNER JOIN USUARIO_PERMISO UP
-ON U.ID_USUARIO = UP.ID_USUARIO
-INNER JOIN ACTIVIDAD_EXTEMPORANEA AE
-ON U.ID_USUARIO = AE.ID_USUARIO;
 
-INSERT INTO USUARIO VALUES (1, 19, 'benjamin.ocotzi', '$2a$04$/pjtunFYAIzCmjBennRE7OsmTaTTytUKtccJR.iM/tSaCdOTDO4O.', 
-'Benjamin Natanael', 'Ocotzi', 'Alvarez', 'Proveedor de Soporte', 'benist.sci@gmail.com', '', 'ROLE_ADMINISTRADOR', 
+INSERT INTO USUARIO VALUES (1, 19, 'benjamin.ocotzi', '$2a$04$/pjtunFYAIzCmjBennRE7OsmTaTTytUKtccJR.iM/tSaCdOTDO4O.',
+'Benjamin Natanael', 'Ocotzi', 'Alvarez', 'Proveedor de Soporte', 'benist.sci@gmail.com', '', 'ROLE_ADMINISTRADOR',
 'martes 11 de abril de 2017', '21:57:35.42');
 
 INSERT INTO USUARIO_PERMISO VALUES(1, 1, true, true, true, true, true);
 
-INSERT INTO ACTIVIDAD_EXTEMPORANEA VALUES(1, 1, false, false, false, false, false);
+--INSERT INTO ACTIVIDAD_EXTEMPORANEA VALUES(1, 1, false, false, false, false, false);
 
 
 INSERT INTO USUARIO VALUES (2, 18, 'ruslan.lopez', '$2a$10$8UnZCnafgrZoSC/WARL5u.usGM0O2MpyGBPg6/hR7LuMKXTslYZtq',
-'Ruslan', 'López', 'López', 'Tester y Diseñador Grafico', 'rustland@gmail.com','','ROLE_ADMINISTRADOR', 
+'Ruslan', 'López', 'López', 'Tester y Diseñador Grafico', 'rustland@gmail.com','','ROLE_ADMINISTRADOR',
 'martes 11 de abril de 2017', '21:57:35.42');
 INSERT INTO USUARIO_PERMISO VALUES(2, 2, true, true, true, true, true);
-INSERT INTO ACTIVIDAD_EXTEMPORANEA VALUES(2, 2, false, false, false, false, false);
+--INSERT INTO ACTIVIDAD_EXTEMPORANEA VALUES(2, 2, false, false, false, false, false);
+SELECT * FROM USUARIO U
+  INNER JOIN USUARIO_PERMISO UP
+  ON U.ID_USUARIO = UP.ID_USUARIO;
