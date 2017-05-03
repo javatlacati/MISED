12fa41abc2071264f82e6021d175b4987eb57b40
Ruslan López <scherzo_16@hotmail.com>
2017-05-03 00:23:01 -0500

    limpieza de codigo

--- src/main/java/gob/senado/ppf/sed/repositorio/unidadapoyo/UnidadApoyoRepositorio.java
+++ src/main/java/gob/senado/ppf/sed/repositorio/unidadapoyo/UnidadApoyoRepositorio.java
@@ -1,23 +1,24 @@
 package gob.senado.ppf.sed.repositorio.unidadapoyo;
 
-import java.util.List;
 import gob.senado.ppf.sed.dto.unidadapoyo.UnidadApoyo;
+
+import java.util.List;
 
 public interface UnidadApoyoRepositorio {
 
-	public abstract boolean altaUnidadApoyo(UnidadApoyo unidadApoyo);
+	boolean altaUnidadApoyo(UnidadApoyo unidadApoyo);
 
-	public abstract boolean bajaUnidadApoyo(long idUnidadApoyo);
+	boolean bajaUnidadApoyo(long idUnidadApoyo);
 
-	public abstract UnidadApoyo buscarUnidadApoyo(long idUnidadApoyo);
+	UnidadApoyo buscarUnidadApoyo(long idUnidadApoyo);
 	
-	public abstract UnidadApoyo buscarUnidadApoyo(String nombreUnidadApoyo);
+	UnidadApoyo buscarUnidadApoyo(String nombreUnidadApoyo);
 	
-	public abstract List<UnidadApoyo> obtenerUnidadesApoyo(); 
+	List<UnidadApoyo> obtenerUnidadesApoyo();
 
-	public abstract boolean actualizarUnidadApoyo(UnidadApoyo unidadApoyo);
+	boolean actualizarUnidadApoyo(UnidadApoyo unidadApoyo);
 
-	public abstract long contarUnidadesApoyo();
+	long contarUnidadesApoyo();
 
 }
 
