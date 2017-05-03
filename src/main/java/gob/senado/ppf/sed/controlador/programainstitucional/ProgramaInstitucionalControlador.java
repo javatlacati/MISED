d56df981fa783c4e9775be3e829fafd2e8b396c7
Ruslan López <scherzo_16@hotmail.com>
2017-05-03 06:37:54 -0500

    Limpieza de codigo.

--- src/main/java/gob/senado/ppf/sed/controlador/programainstitucional/ProgramaInstitucionalControlador.java
+++ src/main/java/gob/senado/ppf/sed/controlador/programainstitucional/ProgramaInstitucionalControlador.java
@@ -1,24 +1,25 @@
 package gob.senado.ppf.sed.controlador.programainstitucional;
 
-import java.util.List;
+import gob.senado.ppf.sed.dto.programainstitucional.ProgramaInstitucional;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
-import gob.senado.ppf.sed.dto.programainstitucional.ProgramaInstitucional;
+
+import java.util.List;
 
 public interface ProgramaInstitucionalControlador {
 
-	public abstract ModelAndView vistaAdministracionProgramasInstitucionales();
+	ModelAndView vistaAdministracionProgramasInstitucionales();
 
-	public abstract HttpEntity<List<ProgramaInstitucional>> obtenerProgramasInstitucionales();
+	HttpEntity<List<ProgramaInstitucional>> obtenerProgramasInstitucionales();
 	
-	public abstract ResponseEntity<ProgramaInstitucional> registroProgramaInstitucional(ProgramaInstitucional programaInstitucional);
+	ResponseEntity<ProgramaInstitucional> registroProgramaInstitucional(ProgramaInstitucional programaInstitucional);
 	
-	public abstract ResponseEntity<ProgramaInstitucional> bajaProgramaInstitucional(String idProgramaInstitucional);
+	ResponseEntity<ProgramaInstitucional> bajaProgramaInstitucional(String idProgramaInstitucional);
 	
-	public abstract HttpEntity<ProgramaInstitucional> buscarProgramaInstitucional(String idProgramaInstitucional);
+	HttpEntity<ProgramaInstitucional> buscarProgramaInstitucional(String idProgramaInstitucional);
 	
-	public abstract ResponseEntity<ProgramaInstitucional> actualizarProgramaInstitucional(ProgramaInstitucional programaInstitucional, BindingResult result);
+	ResponseEntity<ProgramaInstitucional> actualizarProgramaInstitucional(ProgramaInstitucional programaInstitucional, BindingResult result);
 
 }
