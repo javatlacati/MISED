bf090c5c39850d4a7b204f46ad1e20318fc3348b
Benjamin <Benjamin@10.0.13.34>
2017-05-03 18:59:49 -0500

    Actualización de comentarios para las clases ConfiguracionParaExtras y ConfiguracionParaSeguridad

--- src/main/java/gob/senado/ppf/sed/configuracion/ConfiguracionParaExtras.java
+++ src/main/java/gob/senado/ppf/sed/configuracion/ConfiguracionParaExtras.java
@@ -7,17 +7,35 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
+/**
+ * Clase de configuración que se encarga de cargar los archivos yml
+ * para ser posteriormente asignados a las demas clases de configuración.
+ *
+ * @author Benjamin Natanael Ocotzi Alvarez
+ */
 @Configuration
 @PropertySource("classpath:aplicacion/datasource.yml")
 public class ConfiguracionParaExtras {
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer ymlProperties() {
-		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-		YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
-		yaml.setResources(new ClassPathResource("aplicacion/datasource.yml"));
-		pspc.setProperties(yaml.getObject());
-		return pspc;
-	}
-
+    /**
+     * Constructor por defecto de la clase de configuracion ConfiguracionParaExtras.
+     */
+    public ConfiguracionParaExtras() {
+    
+    }
+   
+    /**
+     * Bean de configuración que se encarga de leer todos los properties pero se ha cambiado la implementacion para
+     * que lea archivo .yml de la carpeta aplicacion/.
+     * @return PropertySourcesPlaceholderConfigurer objeto que lee los archivos .yml
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer ymlProperties() {
+        PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("aplicacion/datasource.yml"));
+        pspc.setProperties(yaml.getObject());
+        return pspc;
+    }
+    
 }
--- src/main/java/gob/senado/ppf/sed/configuracion/ConfiguracionParaSeguridad.java
+++ src/main/java/gob/senado/ppf/sed/configuracion/ConfiguracionParaSeguridad.java
@@ -16,21 +16,52 @@ import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import javax.sql.DataSource;
 
+/**
+ * Clase de configuración que define las politicas de acceso hacia los controladores, niveles de acceso mediante roles, login y logout de la aplicación.
+ * @author Benjamin Natanael Ocotzi Alvarez
+ */
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionParaSeguridad extends WebSecurityConfigurerAdapter {
-
-    @Autowired
+    
+    /** Variable que almacena el puente de conexión a la base de datos. */
     private DataSource dataSource;
-
+    
+    /**
+     * Consulta SQL que permite obtener la identidad (nombre de usuario) y la contraseña hasheada de la tabla usuario junto con
+     * sus permisos si la condición de que la identidad sea igual a la del valor literal y si tiene el permiso de autenticarse se cumple.
+     */
     private static final String USUARIOS_POR_USUARIO = "SELECT U.IDENTIDAD, U.CLAVE_ACCESO, UP.PUEDE_AUTENTICARSE FROM USUARIO U " +
             "INNER JOIN USUARIO_PERMISO UP ON U.ID_USUARIO = UP.ID_USUARIO WHERE U.IDENTIDAD = ? AND UP.PUEDE_AUTENTICARSE = true";
-
+    
+    /**
+     * Consulta SQL que permite obtener el rol de usuario de acuerdo a su identidad (nombre de usuario) y si tienen el permiso de autenticarse en la aplicación.
+     */
     private static final String AUTORIDADES_POR_USUARIO = "SELECT U.IDENTIDAD, U.ROL_DESIGNADO FROM USUARIO U INNER JOIN USUARIO_PERMISO UP " +
             "ON U.ID_USUARIO = UP.ID_USUARIO WHERE U.IDENTIDAD = ? AND UP.PUEDE_AUTENTICARSE = true";
-
-    private static final int MAXIMUM_SESSIONS = 1;
-
+    
+    /**
+     * Constante que almacena el limite maximo de sesiones POR USUARIO.
+     */
+    private static final int LIMITE_SESIONES_POR_USUARIO = 1;
+    
+    /**
+     * Constructor de la clase ConfiguracionParaSeguridad que recibe un DataSource para realizar la autenticacion
+     * y autorizacion (Roles y niveles de acceso) en base a su rol.
+     * @param dataSource DataSource puente de conexión a la base de datos.
+     */
+    @Autowired
+    public ConfiguracionParaSeguridad(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    
+    
+    /**
+     * Metodo de configuración que permite especificar que tipo de autenticación, hasheo de contraseñas y consultas se manejaran
+     * en la aplicación para que un usuario tenga acceso.
+     * @param auth AuthenticationManagerBuilder objeto de configuracion que define las politicas de Autenticación
+     * @throws Exception
+     */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
@@ -39,7 +70,7 @@ public class ConfiguracionParaSeguridad extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery(ConfiguracionParaSeguridad.USUARIOS_POR_USUARIO)
                 .authoritiesByUsernameQuery(ConfiguracionParaSeguridad.AUTORIDADES_POR_USUARIO);
     }
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CharacterEncodingFilter filter = new CharacterEncodingFilter("UTF-8", true);
@@ -48,7 +79,7 @@ public class ConfiguracionParaSeguridad extends WebSecurityConfigurerAdapter {
                 .xssProtection()
                 .xssProtectionEnabled(true)
                 .block(true);
-
+        
         http.authorizeRequests()
                 .antMatchers("/administrador/**").hasRole("ADMINISTRADOR")
                 .antMatchers("/inicio-sesion", "/inicio.htm", "/").permitAll()
@@ -71,23 +102,23 @@ public class ConfiguracionParaSeguridad extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().accessDeniedPage("/error/403")
                 .and()
                 .sessionManagement()
-                .maximumSessions(MAXIMUM_SESSIONS)
+                .maximumSessions(LIMITE_SESIONES_POR_USUARIO)
                 .sessionRegistry(sessionRegistry());
     }
-
+    
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/resources/**");
     }
-
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    
     @Bean
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
     }
-
+    
 }
