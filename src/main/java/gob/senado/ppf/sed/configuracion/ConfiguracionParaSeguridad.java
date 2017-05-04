6cf636a447f3131bd0c127dddfcc1328c60b2099
Ruslan López <scherzo_16@hotmail.com>
2017-05-04 02:01:02 -0500

    Corrección de documentación.

--- src/main/java/gob/senado/ppf/sed/configuracion/ConfiguracionParaSeguridad.java
+++ src/main/java/gob/senado/ppf/sed/configuracion/ConfiguracionParaSeguridad.java
@@ -14,53 +14,62 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
 import javax.sql.DataSource;
 
 /**
- * Clase de configuración que define las politicas de acceso hacia los controladores, niveles de acceso mediante roles, login y logout de la aplicación.
+ * Configuraci&oacute;n de seguidad de la aplicaci&oacute;n.
+ * Define las pol&iacute;ticas de acceso hacia los controladores, niveles de acceso mediante roles, login y logout de la aplicaci&oacute;n.
+ *
  * @author Benjamin Natanael Ocotzi Alvarez
  */
 @Configuration
 @EnableWebSecurity
 public class ConfiguracionParaSeguridad extends WebSecurityConfigurerAdapter {
-    
-    /** Variable que almacena el puente de conexión a la base de datos. */
-    private DataSource dataSource;
-    
+
     /**
-     * Consulta SQL que permite obtener la identidad (nombre de usuario) y la contraseña hasheada de la tabla usuario junto con
+     * Puente de conexi&oacute;n a la base de datos.
+     */
+    private DataSource dataSource;
+
+    /**
+     * Consulta que obtiene la identidad (nombre de usuario) y la contraseña hasheada de la tabla usuario junto con
      * sus permisos si la condición de que la identidad sea igual a la del valor literal y si tiene el permiso de autenticarse se cumple.
      */
     private static final String USUARIOS_POR_USUARIO = "SELECT U.IDENTIDAD, U.CLAVE_ACCESO, UP.PUEDE_AUTENTICARSE FROM USUARIO U " +
             "INNER JOIN USUARIO_PERMISO UP ON U.ID_USUARIO = UP.ID_USUARIO WHERE U.IDENTIDAD = ? AND UP.PUEDE_AUTENTICARSE = true";
-    
+
     /**
-     * Consulta SQL que permite obtener el rol de usuario de acuerdo a su identidad (nombre de usuario) y si tienen el permiso de autenticarse en la aplicación.
+     * Consulta que obtiene el rol de usuario de acuerdo a su identidad (nombre de usuario) con el permiso de autenticarse en la aplicaci&oacute;n.
      */
     private static final String AUTORIDADES_POR_USUARIO = "SELECT U.IDENTIDAD, U.ROL_DESIGNADO FROM USUARIO U INNER JOIN USUARIO_PERMISO UP " +
             "ON U.ID_USUARIO = UP.ID_USUARIO WHERE U.IDENTIDAD = ? AND UP.PUEDE_AUTENTICARSE = true";
-    
+
     /**
-     * Constante que almacena el limite maximo de sesiones POR USUARIO.
+     * L&iacute;mite m&aacute;ximo de sesiones por usuario del sistema.
      */
     private static final int LIMITE_SESIONES_POR_USUARIO = 1;
-    
+
     /**
-     * Constructor de la clase ConfiguracionParaSeguridad que recibe un DataSource para realizar la autenticacion
-     * y autorizacion (Roles y niveles de acceso) en base a su rol.
-     * @param dataSource DataSource puente de conexión a la base de datos.
+     * Crea la configuración de seguridad con la ayuda de un orígen de datos.
+     * <p>
+     * Parar el caso de esta aplicación el origen de datos será una base de datos.
+     *
+     * @param dataSource puente de conexión a la base de datos.
      */
     @Autowired
-    public ConfiguracionParaSeguridad(DataSource dataSource){
+    public ConfiguracionParaSeguridad(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    
-    
+
+
     /**
-     * Metodo de configuración que permite especificar que tipo de autenticación, hasheo de contraseñas y consultas se manejaran
-     * en la aplicación para que un usuario tenga acceso.
-     * @param auth AuthenticationManagerBuilder objeto de configuracion que define las politicas de Autenticación
-     * @throws Exception si el DataSource no existe o la consulta SQL esta mal escrita.
+     * Configura el proceso de autentificaci&oacute;n.
+     * Permite especificar que tipo de autenticaci&oacute;n, hasheo de contrase&ntilde;as y consultas se manejar&aacute;n
+     * en la aplicaci&oacute;n para que un usuario tenga acceso.
+     *
+     * @param auth objeto que contiene las pol&iacute;ticas de Autenticaci&oacute;n
+     * @throws Exception si el oigen de la informaci&oacute;n no existe o la consulta SQL esta mal escrita.
      */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -70,12 +79,17 @@ public class ConfiguracionParaSeguridad extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery(ConfiguracionParaSeguridad.USUARIOS_POR_USUARIO)
                 .authoritiesByUsernameQuery(ConfiguracionParaSeguridad.AUTORIDADES_POR_USUARIO);
     }
-    
+
     /**
-     * Metodo de configuración que filtra contenido XSS en una peticion, filtra peticiones de acuerdo
-     * a los roles de usuario, redirecciona a una pagina de inicio si la autenticación es correcta en
-     * caso contrario a otra, se hace un registro de sesiones, se limita a una sesión por usuario y
-     * navegador.
+     * Configura la seguridad de las comunicaciones entre el navegador del usuario y el servidor.
+     * Para ello:
+     * <p>
+     * Filtra contenido XSS en una petici&oacute;n.
+     * Filtra peticiones de acuerdo a los roles de usuario
+     * Redirecciona a una pagina de inicio si la autenticaci&oacute;n es correcta en
+     * caso contrario a otra
+     * Registra de sesiones( se limita a una sesi&oacute;n por usuario y navegador)
+     *
      * @param http objeto que permite configurar la seguridad para las peticiones.
      * @throws Exception
      */
@@ -112,35 +126,38 @@ public class ConfiguracionParaSeguridad extends WebSecurityConfigurerAdapter {
                 .maximumSessions(LIMITE_SESIONES_POR_USUARIO)
                 .sessionRegistry(sessionRegistry());
     }
-    
+
     /**
-     * Metodo de configuración que ignora todo el contenido de la carpeta 
-     * resources (recursos de la aplicacion tales como las hojas de estilos, 
+     * Como parte de la configuración de seguridad ignora recursos que no se debend e mostrar al usuario.
+     * Ignora el contenido de la carpeta resources (recursos de la aplicacion tales como las hojas de estilos,
      * scripts, etc).
+     *
      * @param web
-     * @throws Exception 
+     * @throws Exception
      */
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/resources/**");
     }
-    
+
     /**
-     * Bean de configuración que permite obtener una instancia de un hasheador de cadenas.
-     * @return PasswordEncoder para hashear cadenas de texto.
+     * Obtiene una codificador de cadenas.
+     *
+     * @return Cifrador de contrase&ntilde;as para cadenas de texto.
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     /**
-     * Bean de configuración que permite guardar un registro de las sesiones de usuario.
-     * @return SessionRegistry objeto que almacena el numero de sesiones activas en la aplicación.
+     * Guardar un registro de las sesiones de usuario.
+     *
+     * @return SessionRegistry objeto que almacena el numero de sesiones activas en la aplicaci&oacute;n.
      */
     @Bean
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
     }
-    
+
 }
