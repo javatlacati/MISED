25bc0a47fafb54603b65989860b25a2eba3afe4a
Ruslan López <scherzo_16@hotmail.com>
2017-05-04 01:40:49 -0500

    Limpieza de código

--- src/main/java/gob/senado/ppf/sed/controlador/organodireccionestrategica/OrganoDireccionEstrategicaControlador.java
+++ src/main/java/gob/senado/ppf/sed/controlador/organodireccionestrategica/OrganoDireccionEstrategicaControlador.java
@@ -1,23 +1,24 @@
 package gob.senado.ppf.sed.controlador.organodireccionestrategica;
 
 import gob.senado.ppf.sed.dto.organodireccionestrategica.OrganoDireccionEstrategica;
-import java.util.List;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 public interface OrganoDireccionEstrategicaControlador {
     
-    public abstract ModelAndView vistaAdministracionOrganosDireccionEstrategica();
+    ModelAndView vistaAdministracionOrganosDireccionEstrategica();
     
-    public abstract HttpEntity<List<OrganoDireccionEstrategica>> obtenerOrganosDireccionEstrategica();
+    HttpEntity<List<OrganoDireccionEstrategica>> obtenerOrganosDireccionEstrategica();
     
-    public abstract ResponseEntity<OrganoDireccionEstrategica> registroOrganoDireccionEstrategica(OrganoDireccionEstrategica organoDireccionEstrategica);
+    ResponseEntity<OrganoDireccionEstrategica> registroOrganoDireccionEstrategica(OrganoDireccionEstrategica organoDireccionEstrategica);
     
-    public abstract ResponseEntity<OrganoDireccionEstrategica> bajaOrganoDireccionEstrategica(String idOrganoDireccionEstrategica);
+    ResponseEntity<OrganoDireccionEstrategica> bajaOrganoDireccionEstrategica(String idOrganoDireccionEstrategica);
     
-    public abstract HttpEntity<OrganoDireccionEstrategica> buscarOrganoDireccionEstrategica(String idOrganoDireccionEstrategica);
+    HttpEntity<OrganoDireccionEstrategica> buscarOrganoDireccionEstrategica(String idOrganoDireccionEstrategica);
     
-    public abstract ResponseEntity<OrganoDireccionEstrategica> actualizarOrganosDireccionEstrategica(OrganoDireccionEstrategica organoDireccionEstrategica, BindingResult result);
+    ResponseEntity<OrganoDireccionEstrategica> actualizarOrganosDireccionEstrategica(OrganoDireccionEstrategica organoDireccionEstrategica, BindingResult result);
 }
