992cb392ef31b4aa51a7943d1abb134e37772241
Ruslan López <scherzo_16@hotmail.com>
2017-05-19 11:48:56 -0500

    Se asocian vistas con contolador atendiendo incidencias #2 #3 #4 #5 (Permisos) #6

--- src/main/java/gob/senado/ppf/sed/controlador/usuario/UsuarioControlador.java
+++ src/main/java/gob/senado/ppf/sed/controlador/usuario/UsuarioControlador.java
@@ -1,7 +1,27 @@
 package gob.senado.ppf.sed.controlador.usuario;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 public interface UsuarioControlador {
+    @GetMapping(value = "/usuario/mi-perfil.htm")
     ModelAndView vistaMiPerfil();
+
+    @GetMapping(value = "/administrador/usuario/actualizar-usuarios.htm")
+    ModelAndView vistaActualizarUsuarios();
+
+    @GetMapping(value = "/administrador/usuario/baja-usuarios.htm")
+    ModelAndView vistaBajaUsuarios();
+
+    @GetMapping(value = "/administrador/usuario/busqueda-usuarios.htm")
+    ModelAndView vistaBusquedaUsuarios();
+
+    @GetMapping(value = "/administrador/usuario/captura-usuarios.htm")
+    ModelAndView vistaCapturaUsuarios();
+
+    @GetMapping(value = "/administrador/usuario/permisos-usuarios.htm")
+    ModelAndView vistaPermisosUsuarios();
+
+    @GetMapping(value = "/administrador/usuario/restablecer-cedenciales-usuarios.htm")
+    ModelAndView vistaCredencialesUsuarios();
 }
--- src/main/java/gob/senado/ppf/sed/controlador/usuario/impl/UsuarioControladorImpl.java
+++ src/main/java/gob/senado/ppf/sed/controlador/usuario/impl/UsuarioControladorImpl.java
@@ -6,14 +6,12 @@ import gob.senado.ppf.sed.utilidades.Suppliers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Administrador on 18/05/2017.
  */
 @Controller(value = "usuarioControlador")
-@RequestMapping(value = "/usuario")
 public class UsuarioControladorImpl implements UsuarioControlador {
 
     private final UsuarioServicio usuarioServicio;
@@ -24,9 +22,121 @@ public class UsuarioControladorImpl implements UsuarioControlador {
     }
 
     @Override
-    @GetMapping(value = "/mi-perfil.htm")
+    @GetMapping(value = "/usuario/mi-perfil.htm")
     public ModelAndView vistaMiPerfil() {
         return new ModelAndView("usuario/mi-perfil",
-                "unidadApoyo", Suppliers.USUARIO.get());
+                "usuario", Suppliers.USUARIO.get());
     }
+
+    @Override
+    @GetMapping(value = "/administrador/usuarios/actualizar-usuarios.htm")
+    public ModelAndView vistaActualizarUsuarios() {
+        return new ModelAndView("administrador/usuarios/actualizar-usuarios",
+                "usuario", Suppliers.USUARIO.get());
+    }
+
+    @Override
+    @GetMapping(value = "/administrador/usuarios/baja-usuarios.htm")
+    public ModelAndView vistaBajaUsuarios() {
+        return new ModelAndView("administrador/usuarios/baja-usuarios",
+                "usuario", Suppliers.USUARIO.get());
+    }
+
+    @Override
+    @GetMapping(value = "/administrador/usuarios/busqueda-usuarios.htm")
+    public ModelAndView vistaBusquedaUsuarios() {
+        return new ModelAndView("administrador/usuarios/busqueda-usuarios",
+                "usuario", Suppliers.USUARIO.get());
+    }
+
+    @Override
+    @GetMapping(value = "/administrador/usuarios/captura-usuarios.htm")
+    public ModelAndView vistaCapturaUsuarios() {
+        return new ModelAndView("administrador/usuarios/captura-usuarios",
+                "usuario", Suppliers.USUARIO.get());
+    }
+
+    @Override
+    @GetMapping(value = "/administrador/usuarios/permisos-usuarios.htm")
+    public ModelAndView vistaPermisosUsuarios() {
+        return new ModelAndView("administrador/usuarios/permisos-usuarios",
+                "usuario", Suppliers.USUARIO.get());
+    }
+
+    @Override
+    @GetMapping(value = "/administrador/usuarios/reestablecer-cedenciales-usuarios.htm")
+    public ModelAndView vistaCredencialesUsuarios() {
+        return new ModelAndView("administrador/usuarios/reestablecer-cedenciales-usuarios",
+                "usuario", Suppliers.USUARIO.get());
+    }
+
+//    @Override
+//    @GetMapping(value = "/administrador/usuario/administracion-usuario.htm")
+//    public ModelAndView vistaAdministracionUnidadesApoyo() {
+//        return new ModelAndView("administrador/usuario/administracion-usuario",
+//                "usuario", Suppliers.USUARIO.get());
+//    }
+
+//    @Override
+//    @PostMapping(value = "/administrador/usuario/registro-usuario")
+//    public ResponseEntity<Usuario> registroUsuario(@ModelAttribute("usuario") Usuario usuario) {
+//        if (usuarioServicio.altaUsuario(usuario)) {
+//            return new ResponseEntity<>(usuario, HttpStatus.CREATED);
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+//        }
+//    }
+//
+//    @Override
+//    @PostMapping(value = "/administrador/usuario/baja-usuario/{idUsuario}")
+//    public ResponseEntity<Usuario> bajaUsuario(@PathVariable("idUsuario") String idUsuario) {
+//        try {
+//            long identificadorUsuario = Long.parseLong(idUsuario);
+//            final Usuario usuario = usuarioServicio.buscarUsuario(identificadorUsuario);
+//            if (usuario != null) {
+//                if (usuarioServicio.bajaUsuario(identificadorUsuario)) {
+//                    return new ResponseEntity<>(usuario, HttpStatus.OK);
+//                } else {
+//                    return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+//                }
+//            } else {
+//                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//            }
+//        } catch (NumberFormatException nfe) {
+//            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+//        }
+//    }
+//
+//    @Override
+//    @PostMapping(value = "/administrador/usuario/buscar-usuario/{idUsuario}")
+//    public HttpEntity<Usuario> buscarUnidadApoyo(@PathVariable("idUsuario") String idUsuario) {
+//        try {
+//            long identificadorUsuario = Long.parseLong(idUsuario);
+//            final Usuario usuario = usuarioServicio
+//                    .buscarUsuario(identificadorUsuario);
+//            if (usuario != null) {
+//                return new ResponseEntity<>(usuario, HttpStatus.OK);
+//            } else {
+//                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//            }
+//        } catch (NumberFormatException nfe) {
+//            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+//        }
+//    }
+//
+//    @Override
+//    @PostMapping(value = "/administrador/usuario/actualizar-usuario")
+//    public ResponseEntity<Usuario> actualizarUnidadApoyo(@ModelAttribute("usuario") Usuario usuario, BindingResult result) {
+//        final Usuario usuarioRegistrado = usuarioServicio
+//                .buscarUsuario(usuario.getIdUnidadApoyo());
+//        if (usuarioRegistrado != null) {
+//            if (usuarioServicio.actualizarUsuario(usuario)) {
+//                return new ResponseEntity<>(usuario, HttpStatus.OK);
+//            } else {
+//                return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+//            }
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
 }
