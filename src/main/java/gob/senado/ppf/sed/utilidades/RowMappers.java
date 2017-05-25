14c2ff424566e5c84313b84831d092495b6f3bd2
Ruslan López <scherzo_16@hotmail.com>
2017-05-25 17:35:12 -0500

    Se actualizan rowmappers y suppliers

--- src/main/java/gob/senado/ppf/sed/utilidades/RowMappers.java
+++ src/main/java/gob/senado/ppf/sed/utilidades/RowMappers.java
@@ -1,45 +1,46 @@
 package gob.senado.ppf.sed.utilidades;
 
-import gob.senado.ppf.sed.dto.organizaciontecnicaadministrativa.OrganizacionTecnicaAdministrativa;
-import java.sql.ResultSet;
-import org.springframework.jdbc.core.RowMapper;
 import gob.senado.ppf.sed.dto.organodireccionestrategica.OrganoDireccionEstrategica;
 import gob.senado.ppf.sed.dto.programainstitucional.ProgramaInstitucional;
 import gob.senado.ppf.sed.dto.unidadapoyo.UnidadApoyo;
 import gob.senado.ppf.sed.dto.usuario.Usuario;
-import gob.senado.ppf.sed.dto.usuario.UsuarioPermiso;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
 
 public class RowMappers {
-    
+
     public static final RowMapper<Usuario> ROW_MAPPER_USUARIO = (ResultSet rs, int rowNum) -> {
+
         Usuario u = Suppliers.USUARIO.get();
-        UsuarioPermiso up = Suppliers.USUARIO_PERMISO.get();
-        u.setIdUsuario(rs.getLong(1));
-        u.setIdUnidadApoyo(rs.getLong(2));
-        u.setIdentidad(rs.getString(3));
-        u.setClaveAcceso(rs.getString(4));
-        u.setNombre(rs.getString(5));
-        u.setApellidoPaterno(rs.getString(6));
-        u.setApellidoMaterno(rs.getString(7));
-        u.setPuestoLaboral(rs.getString(8));
-        u.setCorreoElectronico(rs.getString(9));
-        u.setExtensionTelefonica(rs.getString(10));
-        u.setRolDesignado(rs.getString(11));
-        u.setRolDesignado(rs.getString(12));
-        u.setFechaRegistro(rs.getString(13));
-        u.setHoraRegistro(rs.getString(14));
-        up.setIdPermiso(rs.getLong(15));
-        up.setIdUsuario(rs.getLong(16));
-        up.setPuedeConsultar(rs.getBoolean(17));
-        up.setPuedeActualizar(rs.getBoolean(18));
-        up.setPuedeAgregar(rs.getBoolean(19));
-        up.setPuedeBorrar(rs.getBoolean(20));
-        up.setPuedeAutenticarse(rs.getBoolean(21));
-        u.setUsuarioPermiso(up);
+        u.setClaveProgramaInstitucionalAdscrito(rs.getString(1));
+        u.setNombreProgramaInstitucionalAdscrito(rs.getString(2));
+        u.setNombreOrganoDireccionEstrategicaAdscrito(rs.getString(3));
+        u.setNombreUnidadApoyoAdscrito(rs.getString(4));
+        u.setIdUsuario(rs.getLong(5));
+        u.setIdUnidadApoyo(rs.getLong(6));
+        u.setIdentidad(rs.getString(7));
+        u.setClaveAcceso(rs.getString(8));
+        u.setNombre(rs.getString(9));
+        u.setApellidoPaterno(rs.getString(10));
+        u.setApellidoMaterno(rs.getString(11));
+        u.setPuestoLaboral(rs.getString(12));
+        u.setCorreoElectronico(rs.getString(13));
+        u.setExtensionTelefonica(rs.getString(14));
+        u.setRolDesignado(rs.getString(15));
+        u.setTipoUsuario(rs.getString(16));
+        u.setFechaRegistro(rs.getString(17));
+        u.setHoraRegistro(rs.getString(18));
+        u.setPuedeConsultar(rs.getBoolean(19));
+        u.setPuedeActualizar(rs.getBoolean(20));
+        u.setPuedeAgregar(rs.getBoolean(21));
+        u.setPuedeBorrar(rs.getBoolean(22));
+        u.setPuedeAutenticarse(rs.getBoolean(23));
         return u;
     };
-    
-    public static final RowMapper<ProgramaInstitucional> ROW_MAPPER_PROGRAMA_INSTITICIONAL = (ResultSet rs, int rowNum)->{
+
+    public static final RowMapper<ProgramaInstitucional> ROW_MAPPER_PROGRAMA_INSTITICIONAL = (ResultSet rs,
+                                                                                              int rowNum) -> {
         ProgramaInstitucional pi = Suppliers.PROGRAMA_INSTITUCIONAL.get();
         pi.setIdProgramaInstitucional(rs.getLong(1));
         pi.setClave(rs.getString(2));
@@ -49,50 +50,60 @@ public class RowMappers {
         pi.setTotalUnidadesApoyoAsociadasPorOrganosDireccionEstrategicaAdscritos(rs.getLong(6));
         return pi;
     };
-    
-    public static final RowMapper<OrganoDireccionEstrategica> ROW_MAPPER_ORGANO_DIRECCION_ESTRATEGICA = (ResultSet rs, int rowNum)->{
+
+    public static final RowMapper<OrganoDireccionEstrategica> ROW_MAPPER_ORGANO_DIRECCION_ESTRATEGICA = (ResultSet rs, int rowNum) -> {
         OrganoDireccionEstrategica ode = Suppliers.ORGANO_DIRECCION_ESTRATEGICA.get();
-        ode.setIdOrganoDireccionEstrategica(rs.getLong(1));
-        ode.setIdProgramaInstitucional(rs.getLong(2));
-        ode.setNombre(rs.getString(3));
-        ode.setDescripcion(rs.getString(4));
-        ode.setTotalUnidadesApoyoAdscritas(rs.getLong(5));
+        ode.setIdProgramaInstitucional(rs.getLong(1));
+        ode.setClaveProgramaInstitucional(rs.getString(2));
+        ode.setNombreProgramaInstitucional(rs.getString(3));
+        ode.setDescripcionProgramaInstitucional(rs.getString(4));
+        ode.setIdOrganoDireccionEstrategica(rs.getLong(5));
+        ode.setNombre(rs.getString(6));
+        ode.setDescripcion(rs.getString(7));
+        ode.setTotalUnidadesApoyoAdscritas(rs.getLong(8));
         return ode;
     };
-    
-    public static final RowMapper<UnidadApoyo> ROW_MAPPER_UNIDAD_APOYO = (ResultSet rs, int rowNum)->{
+
+    public static final RowMapper<UnidadApoyo> ROW_MAPPER_UNIDAD_APOYO = (ResultSet rs, int rowNum) -> {
         UnidadApoyo ua = Suppliers.UNIDAD_APOYO.get();
-        ua.setIdUnidadApoyo(rs.getLong(1));
-        ua.setIdOrganoDireccionEstrategica(rs.getLong(2));
-        ua.setNombre(rs.getString(3));
-        ua.setProposito(rs.getString(4));
+        ua.setIdProgramaInstitucional(rs.getLong(1));
+        ua.setClaveProgramaInstitucional(rs.getString(2));
+        ua.setNombreProgramaInstitucional(rs.getString(3));
+        ua.setDescripcionProgramaInstitucional(rs.getString(4));
+        ua.setIdOrganoDireccionEstrategica(rs.getLong(5));
+        ua.setNombreOrganoDireccionEstrategica(rs.getString(6));
+        ua.setDescripcionOrganoDireccionEstrategica(rs.getString(7));
+        ua.setIdUnidadApoyo(rs.getLong(8));
+        ua.setNombre(rs.getString(9));
+        ua.setProposito(rs.getString(10));
+        ua.setTotalUsuariosAdscritosUnidadApoyo(rs.getLong(11));
         return ua;
     };
-    
-    public static final RowMapper<OrganizacionTecnicaAdministrativa> ROW_MAPPER_ORGANIZACION_TECNICA_ADMINISTRATIVA = (ResultSet rs, int rowNum)->{
-        ProgramaInstitucional pi = Suppliers.PROGRAMA_INSTITUCIONAL.get();
-        OrganoDireccionEstrategica ode = Suppliers.ORGANO_DIRECCION_ESTRATEGICA.get();
-        UnidadApoyo ua = Suppliers.UNIDAD_APOYO.get();
-        OrganizacionTecnicaAdministrativa ota = Suppliers.ORGANIZACION_TECNICA_ADMINISTRATIVA.get();
-       
-        pi.setIdProgramaInstitucional(rs.getLong(1));
-        pi.setClave(rs.getString(2));
-        pi.setNombre(rs.getString(3));
-        pi.setDescripcion(rs.getString(4));
-        pi.setTotalOrganosDireccionEstrategicaAdscritos(rs.getLong(5));
-        pi.setTotalUnidadesApoyoAsociadasPorOrganosDireccionEstrategicaAdscritos(rs.getLong(6));
-        ode.setIdOrganoDireccionEstrategica(rs.getLong(7));
-        ode.setIdProgramaInstitucional(rs.getLong(8));
-        ode.setNombre(rs.getString(9));
-        ode.setDescripcion(rs.getString(10));
-        ode.setTotalUnidadesApoyoAdscritas(rs.getLong(11));
-        ua.setIdUnidadApoyo(rs.getLong(12));
-        ua.setIdOrganoDireccionEstrategica(rs.getLong(13));
-        ua.setNombre(rs.getString(14));
-        ua.setProposito(rs.getString(15));
-        ota.setProgramInstitucional(pi);
-        ota.setOrganoDireccionEstrategica(ode);
-        ota.setUnidadApoyo(ua);
-        return ota;
-    };
+
+//    public static final RowMapper<OrganizacionTecnicaAdministrativa> ROW_MAPPER_ORGANIZACION_TECNICA_ADMINISTRATIVA = (ResultSet rs, int rowNum)->{
+//        ProgramaInstitucional pi = Suppliers.PROGRAMA_INSTITUCIONAL.get();
+//        OrganoDireccionEstrategica ode = Suppliers.ORGANO_DIRECCION_ESTRATEGICA.get();
+//        UnidadApoyo ua = Suppliers.UNIDAD_APOYO.get();
+//        OrganizacionTecnicaAdministrativa ota = Suppliers.ORGANIZACION_TECNICA_ADMINISTRATIVA.get();
+//
+//        pi.setIdProgramaInstitucional(rs.getLong(1));
+//        pi.setClave(rs.getString(2));
+//        pi.setNombre(rs.getString(3));
+//        pi.setDescripcion(rs.getString(4));
+//        pi.setTotalOrganosDireccionEstrategicaAdscritos(rs.getLong(5));
+//        pi.setTotalUnidadesApoyoAsociadasPorOrganosDireccionEstrategicaAdscritos(rs.getLong(6));
+//        ode.setIdOrganoDireccionEstrategica(rs.getLong(7));
+//        ode.setIdProgramaInstitucional(rs.getLong(8));
+//        ode.setNombre(rs.getString(9));
+//        ode.setDescripcion(rs.getString(10));
+//        ode.setTotalUnidadesApoyoAdscritas(rs.getLong(11));
+//        ua.setIdUnidadApoyo(rs.getLong(12));
+//        ua.setIdOrganoDireccionEstrategica(rs.getLong(13));
+//        ua.setNombre(rs.getString(14));
+//        ua.setProposito(rs.getString(15));
+//        ota.setProgramInstitucional(pi);
+//        ota.setOrganoDireccionEstrategica(ode);
+//        ota.setUnidadApoyo(ua);
+//        return ota;
+//    };
 }
--- src/main/java/gob/senado/ppf/sed/utilidades/Suppliers.java
+++ src/main/java/gob/senado/ppf/sed/utilidades/Suppliers.java
@@ -1,20 +1,17 @@
 package gob.senado.ppf.sed.utilidades;
 
-import gob.senado.ppf.sed.dto.organizaciontecnicaadministrativa.OrganizacionTecnicaAdministrativa;
-import java.util.function.Supplier;
 import gob.senado.ppf.sed.dto.organodireccionestrategica.OrganoDireccionEstrategica;
 import gob.senado.ppf.sed.dto.programainstitucional.ProgramaInstitucional;
 import gob.senado.ppf.sed.dto.unidadapoyo.UnidadApoyo;
 import gob.senado.ppf.sed.dto.usuario.Usuario;
-import gob.senado.ppf.sed.dto.usuario.UsuarioPermiso;
+
+import java.util.function.Supplier;
 
 public final class Suppliers {
-    
     public static final Supplier<Usuario> USUARIO = Usuario::new;
-    public static final Supplier<UsuarioPermiso> USUARIO_PERMISO = UsuarioPermiso::new;
+//    public static final Supplier<UsuarioPermiso> USUARIO_PERMISO = UsuarioPermiso::new;
     public static final Supplier<ProgramaInstitucional> PROGRAMA_INSTITUCIONAL = ProgramaInstitucional::new;
     public static final Supplier<OrganoDireccionEstrategica> ORGANO_DIRECCION_ESTRATEGICA = OrganoDireccionEstrategica::new;
     public static final Supplier<UnidadApoyo> UNIDAD_APOYO = UnidadApoyo::new;
-    public static final Supplier<OrganizacionTecnicaAdministrativa> ORGANIZACION_TECNICA_ADMINISTRATIVA = OrganizacionTecnicaAdministrativa::new;
-    
+//    public static final Supplier<OrganizacionTecnicaAdministrativa> ORGANIZACION_TECNICA_ADMINISTRATIVA = OrganizacionTecnicaAdministrativa::new;
 }
