4d6b82377fa3f495be9351fe53b2bc2fac0ef57a
Ruslan López <scherzo_16@hotmail.com>
2017-05-25 17:38:19 -0500

    Actualización de epositorio de Usuario

--- src/main/java/gob/senado/ppf/sed/repositorio/usuario/UsuarioRepositorio.java
+++ src/main/java/gob/senado/ppf/sed/repositorio/usuario/UsuarioRepositorio.java
@@ -3,28 +3,32 @@ package gob.senado.ppf.sed.repositorio.usuario;
 import gob.senado.ppf.sed.dto.usuario.Usuario;
 import gob.senado.ppf.sed.utilidades.Activacion;
 
+import java.util.List;
+
 public interface UsuarioRepositorio {
 
-	boolean altaUsuario(Usuario usuario);
+    boolean altaUsuario(Usuario usuario);
 
-	boolean actualizarUsuario(Usuario usuario);
+    boolean actualizarUsuario(Usuario usuario);
 
-	boolean bajaUsuario(long idUsuario);
+    boolean bajaUsuario(long idUsuario);
 
-	Usuario buscarUsuario(long idUsuario);
+    Usuario buscarUsuario(long idUsuario);
 
-	Usuario buscarUsuario(String identidad);
-	
-	boolean permitirUsoCuentaUsuario(long idUsuario, Activacion activacion);
-	
-	boolean reestablecerClaveAcceso(long idUsuario, String nuevaClaveAcceso);
+    Usuario buscarUsuario(String identidad);
 
-	long contarUsuarios();
+    List<Usuario> obtenerUsuarios();
 
-	long contarUsuariosPorUnidadApoyo(long idUnidadApoyo);
+    boolean permitirUsoCuentaUsuario(long idUsuario, Activacion activacion);
 
-	long contarUsuariosPorOrganoDireccionEstrategica(long idOrganoDireccionEstrategica);
-	
-	long contarUsuariosPorProgramaInstitucional(long idProgramaInstitucional);
+    boolean reestablecerClaveAcceso(long idUsuario, String nuevaClaveAcceso);
+
+    long contarUsuarios();
+
+    long contarUsuariosPorUnidadApoyo(long idUnidadApoyo);
+
+    long contarUsuariosPorOrganoDireccionEstrategica(long idOrganoDireccionEstrategica);
+
+    long contarUsuariosPorProgramaInstitucional(long idProgramaInstitucional);
 
 }
--- src/main/java/gob/senado/ppf/sed/repositorio/usuario/impl/UsuarioRepositorioImpl.java
+++ src/main/java/gob/senado/ppf/sed/repositorio/usuario/impl/UsuarioRepositorioImpl.java
@@ -11,22 +11,29 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.List;
 
 @Repository(value = "usuarioRepositorio")
 public class UsuarioRepositorioImpl implements UsuarioRepositorio {
 
-    @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UsuarioRepositorioImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public boolean altaUsuario(Usuario usuario) {
         final StringBuilder sqlUsuario = new StringBuilder();
         sqlUsuario.append("INSERT INTO USUARIO (id_unidad_apoyo, identidad, clave_acceso, nombre, ")
                 .append("apellido_paterno, apellido_materno, puesto_laboral, correo_electronico, ")
-                .append("extension_telefonica, rol_designado, tipo_usuario, fecha_registro, hora_registro) ")
-                .append("VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                .append("extension_telefonica, rol_designado, tipo_usuario, fecha_registro, hora_registro, ")
+                .append("puede_consultar, puede_actualizar, puede_agregar, puede_borrar, puede_autenticarse) ")
+                .append("VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); // 18
         KeyHolder keyHolder = new GeneratedKeyHolder();
         int rowsAffectedUsuario = jdbcTemplate.update((Connection con) -> {
             PreparedStatement pst = con.prepareStatement(sqlUsuario.toString(), new String[]{"id_usuario"});
@@ -41,26 +48,16 @@ public class UsuarioRepositorioImpl implements UsuarioRepositorio {
             pst.setString(9, usuario.getExtensionTelefonica());
             pst.setString(10, usuario.getRolDesignado());
             pst.setString(11, usuario.getTipoUsuario());
-            pst.setString(11, usuario.getFechaRegistro());
-            pst.setString(12, usuario.getHoraRegistro());
+            pst.setString(12, usuario.getFechaRegistro());
+            pst.setString(13, usuario.getHoraRegistro());
+            pst.setBoolean(14, usuario.isPuedeConsultar());
+            pst.setBoolean(15, usuario.isPuedeActualizar());
+            pst.setBoolean(16, usuario.isPuedeAgregar());
+            pst.setBoolean(17, usuario.isPuedeBorrar());
+            pst.setBoolean(18, usuario.isPuedeAutenticarse());
             return pst;
         }, keyHolder);
-
-        final long llavePrimariaUsuario = keyHolder.getKey().longValue();
-        final StringBuilder sqlUsuarioPermiso = new StringBuilder();
-        sqlUsuarioPermiso.append("INSERT INTO USUARIO_PERMISO(id_usuario, puede_consultar, ")
-                .append("puede_actualizar, puede_agregar, puede_borrar, puede_autenticarse) VALUES (?, ?, ?, ?, ?, ?)");
-        int rowsAffectedUsuarioPermiso = jdbcTemplate.update((Connection con) -> {
-            PreparedStatement pst = con.prepareStatement(sqlUsuarioPermiso.toString());
-            pst.setLong(1, llavePrimariaUsuario);
-            pst.setBoolean(2, usuario.getUsuarioPermiso().puedeConsultar());
-            pst.setBoolean(3, usuario.getUsuarioPermiso().puedeActualizar());
-            pst.setBoolean(4, usuario.getUsuarioPermiso().puedeAgregar());
-            pst.setBoolean(5, usuario.getUsuarioPermiso().puedeBorrar());
-            pst.setBoolean(6, usuario.getUsuarioPermiso().puedeAutenticarse());
-            return pst;
-        });
-        return rowsAffectedUsuario > 0 && rowsAffectedUsuarioPermiso > 0;
+        return rowsAffectedUsuario > 0;
     }
 
     @Override
@@ -68,8 +65,9 @@ public class UsuarioRepositorioImpl implements UsuarioRepositorio {
         final StringBuilder sqlUsuario = new StringBuilder();
         sqlUsuario.append("UPDATE USUARIO SET id_unidad_apoyo = ?, nombre = ?, ")
                 .append("apellido_paterno = ?, apellido_materno = ?, puesto_laboral = ?, ")
-                .append("correo_electronico = ?, extension_telefonica = ?, rol_designado = ?, ")
-                .append("tipo_usuario = ? WHERE id_usuario = ?");
+                .append("correo_electronico = ?, extension_telefonica = ?, rol_designado = ?, tipo_usuario = ?, ")
+                .append("puede_consultar = ?, puede_actualizar = ?, puede_agregar = ?, puede_borrar = ?, puede_autenticarse = ? ")
+                .append("WHERE id_usuario = ?");
         int rowsAffectedUsuario = jdbcTemplate.update((Connection con) -> {
             PreparedStatement pst = con.prepareStatement(sqlUsuario.toString());
             pst.setLong(1, usuario.getIdUnidadApoyo());
@@ -81,27 +79,15 @@ public class UsuarioRepositorioImpl implements UsuarioRepositorio {
             pst.setString(7, usuario.getExtensionTelefonica());
             pst.setString(8, usuario.getRolDesignado());
             pst.setString(9, usuario.getTipoUsuario());
-            pst.setLong(10, usuario.getIdUsuario());
+            pst.setBoolean(10, usuario.isPuedeConsultar());
+            pst.setBoolean(11, usuario.isPuedeActualizar());
+            pst.setBoolean(12, usuario.isPuedeAgregar());
+            pst.setBoolean(13, usuario.isPuedeBorrar());
+            pst.setBoolean(14, usuario.isPuedeAutenticarse());
+            pst.setLong(15, usuario.getIdUsuario());
             return pst;
         });
-
-        final StringBuilder sqlUsuarioPermiso = new StringBuilder();
-        sqlUsuarioPermiso
-                .append("UPDATE USUARIO_PERMISO SET puede_consultar = ?, puede_actualizar = ?, puede_agregar = ?, ")
-                .append("puede_borrar = ?, puede_autenticarse = ? WHERE id_permiso = ? AND id_usuario = ?");
-        int rowsAffectedUsuarioPermiso = jdbcTemplate.update((Connection con) -> {
-            PreparedStatement pst = con.prepareStatement(sqlUsuarioPermiso.toString());
-            pst.setBoolean(1, usuario.getUsuarioPermiso().puedeConsultar());
-            pst.setBoolean(2, usuario.getUsuarioPermiso().puedeActualizar());
-            pst.setBoolean(3, usuario.getUsuarioPermiso().puedeAgregar());
-            pst.setBoolean(4, usuario.getUsuarioPermiso().puedeBorrar());
-            pst.setBoolean(5, usuario.getUsuarioPermiso().puedeAutenticarse());
-            pst.setLong(6, usuario.getUsuarioPermiso().getIdPermiso());
-            pst.setLong(7, usuario.getUsuarioPermiso().getIdUsuario());
-            return pst;
-        });
-
-        return rowsAffectedUsuario > 0 && rowsAffectedUsuarioPermiso > 0;
+        return rowsAffectedUsuario > 0;
     }
 
     @Override
@@ -113,9 +99,15 @@ public class UsuarioRepositorioImpl implements UsuarioRepositorio {
     public Usuario buscarUsuario(long idUsuario) {
         try {
             StringBuilder sql = new StringBuilder();
-            sql.append("SELECT * FROM USUARIO U INNER JOIN USUARIO_PERMISO UP ON U.ID_USUARIO = UP.ID_USUARIO ").append(
-                    "WHERE U.ID_USUARIO = ?");
-            return jdbcTemplate.queryForObject(sql.toString(), new Object[]{idUsuario}, RowMappers.ROW_MAPPER_USUARIO);
+            sql.append(" SELECT PI.CLAVE AS CLAVE_PROGRAMA_INSTITUCIONAL, PI.NOMBRE AS NOMBRE_PROGRAMA_INSTITUCIONAL, ")
+                    .append("ODE.NOMBRE AS NOMBRE_ORGANO_DIRECCION_ESTRATEGICA, UA.NOMBRE AS NOMBRE_UNIDAD_APOYO, ")
+                    .append("U.* FROM PROGRAMA_INSTITUCIONAL PI INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
+                    .append("ON PI.ID_PROGRAMA_INSTITUCIONAL = ODE.ID_PROGRAMA_INSTITUCIONAL ")
+                    .append("INNER JOIN UNIDAD_APOYO UA ON ODE.ID_ORGANO_DIRECCION_ESTRATEGICA = UA.ID_ORGANO_DIRECCION_ESTRATEGICA ")
+                    .append("INNER JOIN USUARIO U ON UA.ID_UNIDAD_APOYO = U.ID_UNIDAD_APOYO ")
+                    .append("WHERE U.ID_USUARIO = ?");
+            return jdbcTemplate.queryForObject(sql.toString(), new Object[]{idUsuario},
+                    RowMappers.ROW_MAPPER_USUARIO);
         } catch (EmptyResultDataAccessException erdae) {
             return null;
         } catch (IncorrectResultSizeDataAccessException irsdae) {
@@ -127,9 +119,15 @@ public class UsuarioRepositorioImpl implements UsuarioRepositorio {
     public Usuario buscarUsuario(String identidad) {
         try {
             StringBuilder sql = new StringBuilder();
-            sql.append("SELECT * FROM USUARIO U INNER JOIN USUARIO_PERMISO UP ON U.ID_USUARIO = UP.ID_USUARIO ").append(
-                    "WHERE U.IDENTIDAD = ?");
-            return jdbcTemplate.queryForObject(sql.toString(), new Object[]{identidad}, RowMappers.ROW_MAPPER_USUARIO);
+            sql.append(" SELECT PI.CLAVE AS CLAVE_PROGRAMA_INSTITUCIONAL, PI.NOMBRE AS NOMBRE_PROGRAMA_INSTITUCIONAL, ")
+                    .append("ODE.NOMBRE AS NOMBRE_ORGANO_DIRECCION_ESTRATEGICA, UA.NOMBRE AS NOMBRE_UNIDAD_APOYO, ")
+                    .append("U.* FROM PROGRAMA_INSTITUCIONAL PI INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
+                    .append("ON PI.ID_PROGRAMA_INSTITUCIONAL = ODE.ID_PROGRAMA_INSTITUCIONAL ")
+                    .append("INNER JOIN UNIDAD_APOYO UA ON ODE.ID_ORGANO_DIRECCION_ESTRATEGICA = UA.ID_ORGANO_DIRECCION_ESTRATEGICA ")
+                    .append("INNER JOIN USUARIO U ON UA.ID_UNIDAD_APOYO = U.ID_UNIDAD_APOYO ")
+                    .append("WHERE U.IDENTIDAD = ?");
+            return jdbcTemplate.queryForObject(sql.toString(), new Object[]{identidad},
+                    RowMappers.ROW_MAPPER_USUARIO);
         } catch (EmptyResultDataAccessException erdae) {
             return null;
         } catch (IncorrectResultSizeDataAccessException irsdae) {
@@ -138,9 +136,21 @@ public class UsuarioRepositorioImpl implements UsuarioRepositorio {
     }
 
     @Override
+    public List<Usuario> obtenerUsuarios() {
+        StringBuilder sql = new StringBuilder();
+        sql.append(" SELECT PI.CLAVE AS CLAVE_PROGRAMA_INSTITUCIONAL, PI.NOMBRE AS NOMBRE_PROGRAMA_INSTITUCIONAL, ")
+                .append("ODE.NOMBRE AS NOMBRE_ORGANO_DIRECCION_ESTRATEGICA, UA.NOMBRE AS NOMBRE_UNIDAD_APOYO, ")
+                .append("U.* FROM PROGRAMA_INSTITUCIONAL PI INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
+                .append("ON PI.ID_PROGRAMA_INSTITUCIONAL = ODE.ID_PROGRAMA_INSTITUCIONAL ")
+                .append("INNER JOIN UNIDAD_APOYO UA ON ODE.ID_ORGANO_DIRECCION_ESTRATEGICA = UA.ID_ORGANO_DIRECCION_ESTRATEGICA ")
+                .append("INNER JOIN USUARIO U ON UA.ID_UNIDAD_APOYO = U.ID_UNIDAD_APOYO ");
+        return jdbcTemplate.query(sql.toString(), RowMappers.ROW_MAPPER_USUARIO);
+    }
+
+    @Override
     public boolean permitirUsoCuentaUsuario(long idUsuario, Activacion activacion) {
         StringBuilder sql = new StringBuilder();
-        sql.append("UPDATE USUARIO_PERMISO SET puede_autenticarse = ? WHERE id_usuario = ?");
+        sql.append("UPDATE USUARIO SET puede_autenticarse = ? WHERE id_usuario = ?");
         return jdbcTemplate.update(sql.toString(), new Object[]{activacion.getEstadoInterruptor(), idUsuario}) > 0;
     }
 
@@ -172,4 +182,5 @@ public class UsuarioRepositorioImpl implements UsuarioRepositorio {
         return jdbcTemplate.queryForObject("SELECT CONTAR_USUARIOS_POR_PROGRAMA_INSTITUCIONAL(?)",
                 new Object[]{idProgramaInstitucional}, Long.class);
     }
+
 }
