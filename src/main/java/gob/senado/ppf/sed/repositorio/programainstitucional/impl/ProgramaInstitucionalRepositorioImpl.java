d02e6ce2e2919b09acca5c584decced63084b397
Ruslan López <scherzo_16@hotmail.com>
2017-06-14 08:06:02 -0500

    Se hace pueba apra extraer consultas a properties

--- src/main/java/gob/senado/ppf/sed/repositorio/programainstitucional/impl/ProgramaInstitucionalRepositorioImpl.java
+++ src/main/java/gob/senado/ppf/sed/repositorio/programainstitucional/impl/ProgramaInstitucionalRepositorioImpl.java
@@ -1,16 +1,18 @@
 package gob.senado.ppf.sed.repositorio.programainstitucional.impl;
 
 import gob.senado.ppf.sed.dto.organodireccionestrategica.OrganoDireccionEstrategica;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 import gob.senado.ppf.sed.dto.programainstitucional.ProgramaInstitucional;
 import gob.senado.ppf.sed.dto.unidadapoyo.UnidadApoyo;
 import gob.senado.ppf.sed.repositorio.programainstitucional.ProgramaInstitucionalRepositorio;
 import gob.senado.ppf.sed.utilidades.RowMappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository(value = "programaInstitucionalRepositorio")
 public class ProgramaInstitucionalRepositorioImpl implements ProgramaInstitucionalRepositorio {
@@ -21,29 +23,24 @@ public class ProgramaInstitucionalRepositorioImpl implements ProgramaInstitucion
     public ProgramaInstitucionalRepositorioImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    @Autowired
+    private Environment env;
     
     
     @Override
     public boolean altaProgramaInstitucional(ProgramaInstitucional programaInstitucional) {
-        return jdbcTemplate.update("INSERT INTO PROGRAMA_INSTITUCIONAL(CLAVE, NOMBRE, DESCRIPCION) VALUES (?, ?, ?)",
-                new Object[] { programaInstitucional.getClave(), programaInstitucional.getNombre(),
-                    programaInstitucional.getDescripcion() }) > 0;
+        System.out.println(programaInstitucional);
+        return jdbcTemplate.update(env.getProperty("inserta_programa_institucional"),
+                programaInstitucional.getClave(), programaInstitucional.getNombre(),
+                programaInstitucional.getDescripcion()) > 0;
     }
     
     @Override
     public ProgramaInstitucional buscarProgramaInstitucional(long idProgramaInstitucional) {
         try {
-            StringBuilder sql = new StringBuilder();
-            sql.append("SELECT PI.*,(SELECT COUNT(*) FROM ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                    .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                    .append("AS NUMERO_ORGANOS_DIRECCION_ESTRATEGICA_ADSCRITOS, ")
-                    .append("(SELECT COUNT(*) FROM UNIDAD_APOYO UA ")
-                    .append("INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                    .append("ON UA.ID_ORGANO_DIRECCION_ESTRATEGICA = ODE.ID_ORGANO_DIRECCION_ESTRATEGICA ")
-                    .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                    .append("AS NUMERO_UNIDADES_APOYO_ADSCRITAS ")
-                    .append("FROM PROGRAMA_INSTITUCIONAL PI WHERE ID_PROGRAMA_INSTITUCIONAL = ?");
-            return jdbcTemplate.queryForObject(sql.toString(), new Object[] { idProgramaInstitucional },
+
+            return jdbcTemplate.queryForObject(env.getProperty("busca_programa_institucional_por_id"), new Object[] { idProgramaInstitucional },
                     RowMappers.ROW_MAPPER_PROGRAMA_INSTITICIONAL);
         } catch (EmptyResultDataAccessException erdae) {
             return null;
@@ -55,17 +52,7 @@ public class ProgramaInstitucionalRepositorioImpl implements ProgramaInstitucion
     @Override
     public ProgramaInstitucional buscarProgramaInstitucional(String claveProgramaInstitucional) {
         try {
-            StringBuilder sql = new StringBuilder();
-            sql.append("SELECT PI.*,(SELECT COUNT(*) FROM ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                    .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                    .append("AS NUMERO_ORGANOS_DIRECCION_ESTRATEGICA_ADSCRITOS, ")
-                    .append("(SELECT COUNT(*) FROM UNIDAD_APOYO UA ")
-                    .append("INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                    .append("ON UA.ID_ORGANO_DIRECCION_ESTRATEGICA = ODE.ID_ORGANO_DIRECCION_ESTRATEGICA ")
-                    .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                    .append("AS NUMERO_UNIDADES_APOYO_ADSCRITAS ")
-                    .append("FROM PROGRAMA_INSTITUCIONAL PI WHERE CLAVE = ?");
-            return jdbcTemplate.queryForObject(sql.toString(), new Object[] { claveProgramaInstitucional },
+            return jdbcTemplate.queryForObject(env.getProperty("busca_programa_institucional_por_clave"), new Object[] { claveProgramaInstitucional },
                     RowMappers.ROW_MAPPER_PROGRAMA_INSTITICIONAL);
         } catch (EmptyResultDataAccessException erdae) {
             return null;
@@ -76,46 +63,18 @@ public class ProgramaInstitucionalRepositorioImpl implements ProgramaInstitucion
     
     @Override
     public List<ProgramaInstitucional> obtenerProgramasInstitucionales() {
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT PI.*,(SELECT COUNT(*) FROM ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                .append("AS NUMERO_ORGANOS_DIRECCION_ESTRATEGICA_ADSCRITOS, ")
-                .append("(SELECT COUNT(*) FROM UNIDAD_APOYO UA ")
-                .append("INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                .append("ON UA.ID_ORGANO_DIRECCION_ESTRATEGICA = ODE.ID_ORGANO_DIRECCION_ESTRATEGICA ")
-                .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                .append("AS NUMERO_UNIDADES_APOYO_ADSCRITAS ")
-                .append("FROM PROGRAMA_INSTITUCIONAL PI");
-        return jdbcTemplate.query(sql.toString(), RowMappers.ROW_MAPPER_PROGRAMA_INSTITICIONAL);
+        return jdbcTemplate.query(env.getProperty("obtener_programas_institucionales"), RowMappers.ROW_MAPPER_PROGRAMA_INSTITICIONAL);
     }
     
     @Override
     public List<OrganoDireccionEstrategica> obtenerOrganosDireccionEstrategicaPorIdProgramaInstitucional(long idProgramaInstitucional) {
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT ODE.*, (SELECT COUNT(UA.ID_UNIDAD_APOYO) FROM UNIDAD_APOYO UA ")
-                .append("WHERE UA.ID_ORGANO_DIRECCION_ESTRATEGICA = ODE.ID_ORGANO_DIRECCION_ESTRATEGICA) ")
-                .append("AS NUMERO_UNIDADES_APOYO_ADSCRITAS FROM ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = ?");
-        return jdbcTemplate.query(sql.toString(),
+        return jdbcTemplate.query(env.getProperty("obtener_ode_por_idprogramainstitucional"),
                 new Object[]{idProgramaInstitucional}, RowMappers.ROW_MAPPER_ORGANO_DIRECCION_ESTRATEGICA);
     }
     
     @Override
     public List<UnidadApoyo> obtenerUnidadesApoyoPorIdProgramaInstitucional(long idProgramaInstitucional) {
-        final StringBuilder sql = new StringBuilder();
-        sql.append("SELECT UA.ID_UNIDAD_APOYO, UA.ID_ORGANO_DIRECCION_ESTRATEGICA, UA.NOMBRE, UA.PROPOSITO, ")
-                .append("(SELECT COUNT(*) FROM ORGANO_DIRECCION_ESTRATEGICA ODE  ")
-                .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                .append("AS NUMERO_ORGANOS_DIRECCION_ESTRATEGICA_ADSCRITOS, ")
-                .append("(SELECT COUNT(*) FROM UNIDAD_APOYO UA INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                .append("ON UA.ID_ORGANO_DIRECCION_ESTRATEGICA = ODE.ID_ORGANO_DIRECCION_ESTRATEGICA ")
-                .append("WHERE ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL) ")
-                .append("AS NUMERO_UNIDADES_APOYO_ADSCRITAS ")
-                .append("FROM UNIDAD_APOYO UA INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                .append("ON UA.ID_ORGANO_DIRECCION_eSTRATEGICA = ODE.ID_ORGANO_DIRECCION_ESTRATEGICA ")
-                .append("INNER JOIN PROGRAMA_INSTITUCIONAL PI ON ODE.ID_PROGRAMA_INSTITUCIONAL = PI.ID_PROGRAMA_INSTITUCIONAL ")
-                .append("WHERE PI.ID_PROGRAMA_INSTITUCIONAL = ?");
-        return jdbcTemplate.query(sql.toString(), new Object[]{idProgramaInstitucional}, RowMappers.ROW_MAPPER_UNIDAD_APOYO);
+        return jdbcTemplate.query(env.getProperty("obtener_unidadesapoyo_por_idprogramainstitucional"), new Object[]{idProgramaInstitucional}, RowMappers.ROW_MAPPER_UNIDAD_APOYO);
     }
     
     
@@ -123,14 +82,14 @@ public class ProgramaInstitucionalRepositorioImpl implements ProgramaInstitucion
     public boolean actualizarProgramaInstitucional(ProgramaInstitucional pi) {
         return jdbcTemplate.update(
                 "UPDATE PROGRAMA_INSTITUCIONAL SET clave = ?, nombre = ?, descripcion = ? WHERE id_programa_institucional = ?",
-                new Object[] { pi.getClave(), pi.getNombre(), pi.getDescripcion(),
-                    pi.getIdProgramaInstitucional() }) > 0;
+                pi.getClave(), pi.getNombre(), pi.getDescripcion(),
+                pi.getIdProgramaInstitucional()) > 0;
     }
     
     @Override
     public boolean bajaProgramaInstitucional(long idProgramaInstitucional) {
         return jdbcTemplate.update("DELETE FROM PROGRAMA_INSTITUCIONAL WHERE id_programa_institucional = ?",
-                new Object[] { idProgramaInstitucional }) > 0;
+                idProgramaInstitucional) > 0;
     }
     
     @Override
---
+++ src/main/java/gob/senado/ppf/sed/utilidades/Consultas.java
@@ -0,0 +1,13 @@
+package gob.senado.ppf.sed.utilidades;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by Administrador on 13/06/2017.
+ */
+@Configuration
+@PropertySource("classpath:aplicacion/queries.properties")
+public class Consultas {
+
+}
