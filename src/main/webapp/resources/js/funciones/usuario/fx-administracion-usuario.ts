8be8e91fad57e27640df35042ebb5c0e41efe31f
Ruslan López <scherzo_16@hotmail.com>
2017-06-14 23:43:06 -0500

    scripts para administracion de usuario

---
+++ src/main/webapp/resources/js/funciones/usuario/fx-administracion-usuario.ts
@@ -0,0 +1,284 @@
+let token = $("meta[name='_csrf']").attr("content");
+
+const webSocketUsuarios = new WebSocket('ws://localhost:8080/MISED/actualizacion-usuario');
+let graficaUsuariosActivos;
+
+(function() {
+    graficaUsuariosActivos = new Highcharts.Chart({
+        chart: {
+            renderTo: 'grafica-usuarios-activos',
+            type: 'column'
+        },
+        title: {
+            text: 'Número de Usuarios por Unidad de Apoyo'
+        },
+        subtitle: {
+            text: 'Fuente: Módulo de estadística de MISED'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '10px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            allowDecimals: false,
+            title: {
+                text: 'Número de usuarios'
+            }
+        },
+        tooltip: {
+            pointFormat: '{point.name} tiene <b>{point.y} usuarios.</b>'
+        },
+        series: [{
+            name: 'Numero de usuarios por unidad de apoyo',
+            data: [],
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{series.name}',
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '18px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }],
+        credits: {
+            enabled: false
+        },
+        legend: {
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            enabled: false
+        }
+    });
+
+
+    cargarGraficaUsuariosActivos();
+
+    tablaUsuarios = $('#tabla-usuarios')
+        .DataTable({
+            "language": LENGUAJE,
+            dom: 'Bfrtilp',
+            responsive: true,
+            "pagingType": "full_numbers",
+            "lengthMenu": [
+                [5, 15, 25, 50],
+                [5, 15, 25, 50]
+            ],
+            "sAjaxDataProp": "",
+            "bProcessing": true,
+            "aoColumns": [{
+                "mData": "claveProgramaInstitucionalAdscrito",
+                "sDefaultContent": ''
+            }, {
+                "mData": "nombreProgramaInstitucionalAdscrito",
+                "sDefaultContent": ''
+            }, {
+                "mData": "nombreOrganoDireccionEstrategicaAdscrito",
+                "sDefaultContent": ''
+            }, {
+                "mData": "nombreUnidadApoyoAdscrito",
+                "sDefaultContent": ''
+            }, {
+                "mData": "identidad",
+                "sDefaultContent": ''
+            }, {
+                "mData": "nombre",
+                "sDefaultContent": ''
+            }, {
+                "mData": "apellidoPaterno",
+                "sDefaultContent": ''
+            }, {
+                "mData": "apellidoMaterno",
+                "sDefaultContent": ''
+            }, {
+                "mData": "puestoLaboral",
+                "sDefaultContent": ''
+            }, {
+                "mData": "extensionTelefonica",
+                "sDefaultContent": ''
+            }, {
+                "mData": "correoElectronico",
+                "sDefaultContent": ''
+            }, {
+                "mData": "rolDesignado",
+                "sDefaultContent": ''
+            }, {
+                "mData": "tipoUsuario",
+                "sDefaultContent": ''
+            }, {
+                "mData": "fechaRegistro",
+                "sDefaultContent": ''
+            }, {
+                "mData": "horaRegistro",
+                "sDefaultContent": ''
+            }, {
+                "mData": "puedeConsultar",
+                "sDefaultContent": ''
+            }, {
+                "mData": "puedeActualizar",
+                "sDefaultContent": ''
+            }, {
+                "mData": "puedeAgregar",
+                "sDefaultContent": ''
+            }, {
+                "mData": "puedeBorrar",
+                "sDefaultContent": ''
+            }, {
+                "mData": "puedeAutenticarse",
+                "sDefaultContent": ''
+            }, {
+                "mData": "idUsuario",
+                "sDefaultContent": ''
+            }],
+            "columnDefs": [{
+                "targets": [15, 16, 17, 18, 19],
+                "searchable": false,
+                "orderable": false,
+                "data": "acciones",
+                "render": function(data, type, row) {
+                    return (data === "true" || data === true) ? "<i class='material-icons'>check_circle</i>" : "<i class='material-icons'>highlight_off</i>";
+                }
+            }, {
+                "targets": [11],
+                "searchable": false,
+                "orderable": false,
+                "data": "acciones",
+                "render": function(data, type, row) {
+                    switch (data) {
+                        case 'ROLE_ADMINISTRADOR':
+                            return "Administrador";
+                        case 'ROLE_USUARIO':
+                            return "Usuario";
+                    }
+                }
+            },{
+                "targets": 20,
+                "data": "acciones",
+                "render": function(data, type, row) {
+                    return "<a class='btn bg-deep-orange waves-effect' style='margin-right: 7px;' onclick='actualizacionUsuario(" + data + ")'>Actualizar</a>"
+                        + "<a class='btn bg-red waves-effect' style='margin-right: 7px;' onclick='bajaUsuario(" + data + ")'>Eliminar</a>"
+
+                        + "<a id='btn-activacion-"+data+"' class='btn bg-deep-purple waves-effect' style='margin-right: 7px;' onclick='activarCuentaUsuario(" + data + ")'></a>"
+
+                        + "<a class='btn bg-teal waves-effect' style='margin-right: 7px;' onclick='reestablecerCredencialesUsuario(" + data + ")'>Reestablecer credenciales</a>";
+                }
+            }],
+            "rowCallback": function(row, data, index) {
+                if(data.puedeAutenticarse === true || data.puedeAutenticarse === 'true'){
+                    $("#btn-activacion-"+data.idUsuario).text("Activar");
+                }else{
+                    $("#btn-activacion-"+data.idUsuario).text("Desactivar");
+                }
+
+                if (data.puedeAutenticarse === false || data.puedeAutenticarse === 'false') {
+                    $('td', row).css('background-color', '#F44336');
+                }
+
+                if (data.rolDesignado === 'ROLE_ADMINISTRADOR') {
+                    $('td', row).css('background-color', '#ffd700');
+                }
+            },
+            "ajax": {
+                url: "usuarios.json?_csrf=" + token,
+                type: "POST"
+            }
+        });
+
+
+    webSocketUsuarios.onmessage = function(mensaje) {
+        if (mensaje.data === 'actualizacion') {
+            cargarGraficaUsuariosActivos();
+            tablaUsuarios.clear().draw();
+            tablaUsuarios.ajax.reload(null, false);
+        }
+    };
+
+})();
+
+
+
+function cargarGraficaUsuariosActivos() {
+    $.ajax({
+        url: 'numero-usuarios-por-unidad-apoyo.json?_csrf=' + token,
+        type: 'POST',
+        success: function(datosGrafica) {
+            graficaUsuariosActivos.series[0].setData(datosGrafica, true);
+        },
+        error: function() {
+            swal("ERROR", "Se ha perdido la comunicación con el servidor, intentelo mas tarde.");
+        }
+    });
+}
+
+
+function actualizacionUsuario(idUsuario){
+    alert(idUsuario);
+}
+
+function bajaUsuario(idUsuario){
+    swal({
+        title: "¿Estás realmente seguro de borrar este usuario?",
+        text: "Se borrara este usuario permanentemente con todo su historial de actividades(NO se borraran Arboles de objetivos, Indicadores, Calendarizaciones, Informes cuatrimestrales ó cuadros de mando)!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Si, borrar este usuario!",
+        closeOnConfirm: false
+    }, function() {
+        $.ajax({
+            url: 'baja-usuario/' + idUsuario + '?_csrf=' + token,
+            type: 'POST',
+            data: {
+                'idUsuario': idUsuario
+            },
+            success: function(u) {
+                swal("TAREA EXITOSA!", "Se ha correctamente dado de baja el usuario: "
+                    + u.nombre+ " " + u.apellidoPaterno + " " + u.apellidoMaterno + " con nombre de usuario: " + u.identidad);
+                webSocketUsuarios.send('actualizacion');
+            },
+            error: function() {
+                swal("ERROR", "Se ha perdido la comunicación con el servidor ó el recurso que busca ya no existe!, intentelo mas tarde.");
+            }
+        });
+    });
+}
+
+function activarCuentaUsuario(idUsuario){
+    swal({
+        title: "¿Estás realmente seguro de activar/desactivar este usuario?",
+        text: "Al activar o desactivar este usuario afectara solamente si el usuario puede autenticarse o no!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Si, activar/desactivar este usuario!",
+        closeOnConfirm: false
+    }, function() {
+        $.ajax({
+            url: 'activacion-cuenta/' + idUsuario + '?_csrf=' + token,
+            type: 'POST',
+            data: {
+                'idUsuario': idUsuario
+            },
+            success: function(u) {
+                swal("TAREA EXITOSA!", "Se ha realizado la tarea exitosamente!");
+                webSocketUsuarios.send('actualizacion');
+            },
+            error: function() {
+                swal("ERROR", "Se ha perdido la comunicación con el servidor ó el recurso que busca ya no existe!, intentelo mas tarde.");
+            }
+        });
+    });
+}
+
+function reestablecerCredencialesUsuario(idUsuario){
+    alert(idUsuario);
+}
---
+++ src/main/webapp/resources/js/funciones/usuario/fx-captura-usuario.ts
@@ -0,0 +1,173 @@
+var token = $("meta[name='_csrf']").attr("content");
+var webSocketUsuarios = null;
+(function () {
+    cargarUnidadesApoyo();
+    webSocketUsuarios = new WebSocket('ws://localhost:8080/MISED/actualizacion-usuario');
+})();
+function cargarUnidadesApoyo() {
+    $.ajax({
+        url: '../unidad-apoyo/unidades-apoyo.json?_csrf=' + token,
+        type: 'POST',
+        success: function (unidadesApoyo) {
+            var opciones = "";
+            unidadesApoyo.forEach(function (ua) {
+                opciones += "<option data-subtext='" + ua.nombreOrganoDireccionEstrategica +
+                    "' value='" + ua.idUnidadApoyo + "'>" + ua.nombre + "</option>";
+            });
+            $("#selector-id-unidadApoyo").html(opciones);
+            $("#selector-id-unidadApoyo").selectpicker('refresh');
+        },
+        error: function () {
+            swal("ERROR", "Se ha perdido la comunicacion con el servidor " +
+                "o el recurso que busca ya no existe!, intentelo mas tarde.");
+        }
+    });
+}
+function capturarUsuario() {
+    var idUnidadApoyo = $("#selector-id-unidadApoyo").val();
+    var identidad = $("#identidad").val();
+    var claveAcceso = $("#clave-acceso").val();
+    var verificacionClaveAcceso = $("#verificacion-claveAcceso").val();
+    var nombre = $("#nombre").val();
+    var apellidoPaterno = $("#apellido-paterno").val();
+    var apellidoMaterno = $("#apellido-materno").val();
+    var puestoLaboral = $("#puesto-laboral").val();
+    var correoElectronico = $("#correo-electronico").val();
+    var extensionTelefonica = $("#extension-telefonica").val();
+    var rol = $("#selector-rol-usuario").val();
+    var permisos = $("#selector-permisos-usuario").val();
+    var tipo = $("#selector-tipo-usuario").val();
+    var usuario = new Usuario(0, idUnidadApoyo, identidad, claveAcceso, nombre, apellidoPaterno, apellidoMaterno, puestoLaboral, correoElectronico, extensionTelefonica, rol, tipo, false, false, false, false);
+    computarPermisosUsuario(permisos, usuario);
+    if (validarCamposUsuario(usuario, verificacionClaveAcceso)) {
+        $.ajax({
+            url: 'registro-usuario?_csrf=' + token,
+            type: 'POST',
+            data: {
+                'idUnidadApoyo': usuario.idUnidadApoyo,
+                'identidad': usuario.identidad,
+                'claveAcceso': usuario.claveAcceso,
+                'nombre': usuario.nombre,
+                'apellidoPaterno': usuario.apellidoPaterno,
+                'apellidoMaterno': usuario.apellidoMaterno,
+                'puestoLaboral': usuario.puestoLaboral,
+                'correoElectronico': usuario.correoElectronico,
+                'extensionTelefonica': usuario.extensionTelefonica,
+                'rolDesignado': usuario.rolDesignado,
+                'tipoUsuario': usuario.tipoUsuario,
+                'puedeConsultar': usuario.puedeConsultar,
+                'puedeActualizar': usuario.puedeActualizar,
+                'puedeAgregar': usuario.puedeAgregar,
+                'puedeBorrar': usuario.puedeBorrar
+            },
+            success: function (u) {
+                swal("TAREA EXITOSA!", "Se ha registrado el usuario : " + u.identidad);
+                $('#frm-registro-usuario').find('input[type="text"]').val('');
+                webSocketUsuarios.send('actualizacion');
+            },
+            error: function () {
+                swal("ERROR", "Se ha perdido la comunicación con el servidor ó el " +
+                    "nombre de usuario esta duplicado y se evito la actualización!, intentelo mas tarde.");
+            }
+        });
+    }
+}
+function computarPermisosUsuario(permisos, usuario) {
+    if (permisos !== null) {
+        for (var indice = 0; indice < permisos.length; indice++) {
+            switch (permisos[indice]) {
+                case 'CONSULTAR':
+                    usuario.puedeConsultar = true;
+                    break;
+                case 'ACTUALIZAR':
+                    usuario.puedeActualizar = true;
+                    break;
+                case 'AGREGAR':
+                    usuario.puedeAgregar = true;
+                    break;
+                case 'BORRAR':
+                    usuario.puedeBorrar = true;
+                    break;
+            }
+        }
+    }
+}
+function validarCamposUsuario(usuario, verificacionClaveAcceso) {
+    let expresionEmailCorrecto = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (usuario.idUnidadApoyo === '' || usuario.idUnidadApoyo === 0) {
+        swal("ERROR", "El recurso que busca ya no existe!");
+        return false;
+    }
+    else if (usuario.identidad === '' || usuario.identidad.length === 0) {
+        swal("ERROR", "Se requiere que el nombre de usuario no este en blanco!");
+        return false;
+    }
+    else if (usuario.identidad > 18) {
+        swal("ERROR", "Se requiere que el nombre de usuario no tenga mas de 18 caracteres!");
+        return false;
+    }
+    else if (usuario.claveAcceso !== verificacionClaveAcceso) {
+        swal("ERROR", "Es necesario de las contraseñas coincidan!");
+        return false;
+    }
+    else if ((usuario.claveAcceso === '' || usuario.claveAcceso.length === 0)
+        || (verificacionClaveAcceso === '' || verificacionClaveAcceso.length === 0)) {
+        swal("ERROR", "Se requiere que las contraseñas no este en blanco!");
+        return false;
+    }
+    else if (usuario.nombre === '' || usuario.nombre.length === 0) {
+        swal("ERROR", "Se requiere que el nombre no este en blanco!");
+        return false;
+    }
+    else if (usuario.nombre.length > 50) {
+        swal("ERROR", "El nombre no puede sobrepasar los 50 caracteres!");
+        return false;
+    }
+    else if (usuario.apellidoPaterno === '' || usuario.apellidoPaterno.length === 0) {
+        swal("ERROR", "Se requiere que el apellido paterno no este en blanco!");
+        return false;
+    }
+    else if (usuario.apellidoPaterno.length > 50) {
+        swal("ERROR", "El apellido paterno no puede pasar de los 50 caracteres!");
+        return false;
+    }
+    else if (usuario.apellidoMaterno === '' || usuario.apellidoMaterno.length === 0) {
+        swal("ERROR", "Se requiere que el apellido materno no este en blanco!");
+        return false;
+    }
+    else if (usuario.apellidoMaterno.length > 50) {
+        swal("ERROR", "El apellido materno no puede pasar de los 50 caracteres!");
+        return false;
+    }
+    else if (usuario.puestoLaboral === '' || usuario.puestoLaboral.length === 0) {
+        swal("ERROR", "Se requiere que el puesto laboral no este en blanco!");
+        return false;
+    }
+    else if (usuario.puestoLaboral.length > 120) {
+        swal("ERROR", "El puesto laboral no puede pasar de los 120 caracteres!");
+        return false;
+    }
+    else if (usuario.correoElectronico === '' || usuario.correoElectronico.length === 0) {
+        swal("ERROR", "Se requiere que el E-mail no este en blanco!");
+        return false;
+    }
+    else if (!(expresionEmailCorrecto.test(usuario.correoElectronico))) {
+        swal("ERROR", "Se requiere que el E-mail este bien escrito!");
+        return false;
+    }
+    else if (usuario.correoElectronico.length > 120) {
+        swal("ERROR", "Se requiere que el E-mail no tenga mas de 120 caracteres!");
+        return false;
+    }
+    else if (usuario.rolDesignado === '' || usuario.rolDesignado.length === 0) {
+        swal("ERROR", "Se necesita que seleccione un rol de usuario!");
+        return false;
+    }
+    else if (usuario.tipoUsuario === '' || usuario.tipoUsuario.length === 0) {
+        swal("ERROR", "Se requiere que seleccion un tipo de usuario!");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
