cea14e86e140eb0bf942701d0ecf34e84eb8a6b0
Ruslan López <scherzo_16@hotmail.com>
2017-06-17 11:58:13 -0500

    Se extraen consultas de UA

--- src/main/java/gob/senado/ppf/sed/repositorio/unidadapoyo/impl/UnidadApoyoRepositorioImpl.java
+++ src/main/java/gob/senado/ppf/sed/repositorio/unidadapoyo/impl/UnidadApoyoRepositorioImpl.java
@@ -4,6 +4,7 @@ import gob.senado.ppf.sed.dto.unidadapoyo.UnidadApoyo;
 import gob.senado.ppf.sed.repositorio.unidadapoyo.UnidadApoyoRepositorio;
 import gob.senado.ppf.sed.utilidades.RowMappers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,37 +17,38 @@ public class UnidadApoyoRepositorioImpl implements UnidadApoyoRepositorio {
 
     private final JdbcTemplate jdbcTemplate;
 
+    private Environment env;
+
     @Autowired
-    public UnidadApoyoRepositorioImpl(JdbcTemplate jdbcTemplate) {
+    public UnidadApoyoRepositorioImpl(JdbcTemplate jdbcTemplate, Environment env) {
         this.jdbcTemplate = jdbcTemplate;
+        this.env = env;
     }
 
     @Override
     public boolean altaUnidadApoyo(UnidadApoyo ua) {
         return jdbcTemplate.update(
-                "INSERT INTO UNIDAD_APOYO(id_organo_direccion_estrategica, nombre, proposito) VALUES (?, ?, ?)",
-                ua.getIdOrganoDireccionEstrategica(), ua.getNombre(), ua.getProposito()) > 0;
+                env.getProperty("alta_ua"),
+                ua.getIdOrganoDireccionEstrategica(), ua.getNombre(), ua.getProposito()
+        ) > 0;
     }
 
     @Override
     public boolean bajaUnidadApoyo(long idUnidadApoyo) {
-        return jdbcTemplate.update("DELETE FROM UNIDAD_APOYO WHERE id_unidad_apoyo = ?", idUnidadApoyo) > 0;
+        return jdbcTemplate.update(
+                env.getProperty("baja_ua")
+                , idUnidadApoyo
+        ) > 0;
     }
 
     @Override
     public UnidadApoyo buscarUnidadApoyo(long idUnidadApoyo) {
         try {
-            StringBuilder sql = new StringBuilder();
-            sql.append("SELECT PI.ID_PROGRAMA_INSTITUCIONAL, PI.CLAVE, PI.NOMBRE, PI.DESCRIPCION, ")
-                    .append("ODE.ID_ORGANO_DIRECCION_ESTRATEGICA, ODE.NOMBRE, ODE.DESCRIPCION, ")
-                    .append("UA.ID_UNIDAD_APOYO, UA.NOMBRE, UA.PROPOSITO, (SELECT COUNT(*) FROM USUARIO U ")
-                    .append("WHERE UA.ID_UNIDAD_APOYO = U.ID_UNIDAD_APOYO) AS TOTAL_USUARIOS_ADSCRITOS_UNIDAD_APOYO ")
-                    .append("FROM PROGRAMA_INSTITUCIONAL PI INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                    .append("ON PI.ID_PROGRAMA_INSTITUCIONAL = ODE.ID_PROGRAMA_INSTITUCIONAL INNER JOIN UNIDAD_APOYO UA ")
-                    .append("ON ODE.ID_ORGANO_DIRECCION_ESTRATEGICA = UA.ID_ORGANO_DIRECCION_ESTRATEGICA ")
-                    .append("WHERE UA.ID_UNIDAD_APOYO = ?");
-            return jdbcTemplate.queryForObject(sql.toString(),
-                    new Object[]{idUnidadApoyo}, RowMappers.ROW_MAPPER_UNIDAD_APOYO);
+            return jdbcTemplate.queryForObject(
+                    env.getProperty("busca_ua_por_id"),
+                    new Object[]{idUnidadApoyo},
+                    RowMappers.ROW_MAPPER_UNIDAD_APOYO
+            );
         } catch (EmptyResultDataAccessException erdae) {
             return null;
         } catch (IncorrectResultSizeDataAccessException irsdae) {
@@ -57,17 +59,11 @@ public class UnidadApoyoRepositorioImpl implements UnidadApoyoRepositorio {
     @Override
     public UnidadApoyo buscarUnidadApoyo(String nombreUnidadApoyo) {
         try {
-            StringBuilder sql = new StringBuilder();
-            sql.append("SELECT PI.ID_PROGRAMA_INSTITUCIONAL, PI.CLAVE, PI.NOMBRE, PI.DESCRIPCION, ")
-                    .append("ODE.ID_ORGANO_DIRECCION_ESTRATEGICA, ODE.NOMBRE, ODE.DESCRIPCION, ")
-                    .append("UA.ID_UNIDAD_APOYO, UA.NOMBRE, UA.PROPOSITO, (SELECT COUNT(*) FROM USUARIO U ")
-                    .append("WHERE UA.ID_UNIDAD_APOYO = U.ID_UNIDAD_APOYO) AS TOTAL_USUARIOS_ADSCRITOS_UNIDAD_APOYO ")
-                    .append("FROM PROGRAMA_INSTITUCIONAL PI INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                    .append("ON PI.ID_PROGRAMA_INSTITUCIONAL = ODE.ID_PROGRAMA_INSTITUCIONAL INNER JOIN UNIDAD_APOYO UA ")
-                    .append("ON ODE.ID_ORGANO_DIRECCION_ESTRATEGICA = UA.ID_ORGANO_DIRECCION_ESTRATEGICA ")
-                    .append("WHERE UA.NOMBRE = ?");
-            return jdbcTemplate.queryForObject(sql.toString(),
-                    new Object[]{nombreUnidadApoyo}, RowMappers.ROW_MAPPER_UNIDAD_APOYO);
+            return jdbcTemplate.queryForObject(
+                    env.getProperty("busca_ua_por_nombre"),
+                    new Object[]{nombreUnidadApoyo},
+                    RowMappers.ROW_MAPPER_UNIDAD_APOYO
+            );
         } catch (EmptyResultDataAccessException erdae) {
             return null;
         } catch (IncorrectResultSizeDataAccessException irsdae) {
@@ -77,28 +73,24 @@ public class UnidadApoyoRepositorioImpl implements UnidadApoyoRepositorio {
 
     @Override
     public List<UnidadApoyo> obtenerUnidadesApoyo() {
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT PI.ID_PROGRAMA_INSTITUCIONAL, PI.CLAVE, PI.NOMBRE, PI.DESCRIPCION, ")
-                .append("ODE.ID_ORGANO_DIRECCION_ESTRATEGICA, ODE.NOMBRE, ODE.DESCRIPCION, ")
-                .append("UA.ID_UNIDAD_APOYO, UA.NOMBRE, UA.PROPOSITO, (SELECT COUNT(*) FROM USUARIO U ")
-                .append("WHERE UA.ID_UNIDAD_APOYO = U.ID_UNIDAD_APOYO) AS TOTAL_USUARIOS_ADSCRITOS_UNIDAD_APOYO ")
-                .append("FROM PROGRAMA_INSTITUCIONAL PI INNER JOIN ORGANO_DIRECCION_ESTRATEGICA ODE ")
-                .append("ON PI.ID_PROGRAMA_INSTITUCIONAL = ODE.ID_PROGRAMA_INSTITUCIONAL INNER JOIN UNIDAD_APOYO UA ")
-                .append("ON ODE.ID_ORGANO_DIRECCION_ESTRATEGICA = UA.ID_ORGANO_DIRECCION_ESTRATEGICA ORDER BY UA.NOMBRE");
-        return jdbcTemplate.query(sql.toString(), RowMappers.ROW_MAPPER_UNIDAD_APOYO);
+        return jdbcTemplate.query(env.getProperty("obtener_ua"), RowMappers.ROW_MAPPER_UNIDAD_APOYO);
     }
 
     @Override
     public boolean actualizarUnidadApoyo(UnidadApoyo ua) {
         return jdbcTemplate.update(
-                "UPDATE unidad_apoyo SET id_organo_direccion_estrategica = ?, nombre = ?, proposito = ? WHERE id_unidad_apoyo = ?",
+                env.getProperty("actualizar_ua"),
                 new Object[]{ua.getIdOrganoDireccionEstrategica(), ua.getNombre(), ua.getProposito(),
-                        ua.getIdUnidadApoyo()}) > 0;
+                        ua.getIdUnidadApoyo()}
+        ) > 0;
     }
 
     @Override
     public long contarUnidadesApoyo() {
-        return jdbcTemplate.queryForObject("SELECT TOTAL_CONTEO FROM CONTAR_UNIDADES_APOYO", Long.class);
+        return jdbcTemplate.queryForObject(
+                env.getProperty("contar_ua"),
+                Long.class
+        );
     }
 
 }
